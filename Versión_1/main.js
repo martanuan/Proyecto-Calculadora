eaaa24efc1ab2a0cd5e0d8dd76df7f39afb8896a
martanuan <martanyp@hotmail.com>
2023-11-26 18:53:44 +0100

    BUG. Al poner un número teniendo la pantalla un 0, esta se concatenaba.

--- Versión_1/main.js
+++ Versión_1/main.js
@@ -1,14 +1,20 @@
 // Función que nos permite rellenar la pantalla
 // document.calculator.displayPanel.value (es lo que se muestra en la pantalla)
+//Añado
 
 function fillDisplay(inputvalue) {
+    //Verificamos si la pantalla tiene un '0'. En caso afirmativo vaciamos la pantalla (cadena vacía '')
+    if (document.calculator.displayPanel.value === '0')
+        document.calculator.displayPanel.value = '';//Vaciamos la pantalla
+    //Rellenamos en la pantalla la opción pulsada que viene definido en el parámetro inputValue
     document.calculator.displayPanel.value += inputvalue;
 }
-
 // Función que realiza las diferentes operaciones
-    // Si la pantalla es distinta a vacio, hacemos la operación.
+// Si la pantalla es distinta a vacio, hacemos la operación.
 function doOperation () {
-    if (document.calculator.displayPanel.value != "") { 
+    //Verificamos que la pantalla no sea vacía. Si no lo es realizamos la operación mediante el eval.
+    if (document.calculator.displayPanel.value != "") {
+        //eval es una función que evalúa lo que se le pase por parámetro. eval("5+3") --> 8 
         document.calculator.displayPanel.value = eval(document.calculator.displayPanel.value);
     }
 }
