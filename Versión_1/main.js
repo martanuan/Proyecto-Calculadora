69b68abd8967587b0e2dbeaf6996c1433ba8fd50
martanuan <martanyp@hotmail.com>
2023-11-27 23:40:40 +0100

    MEJORAS. cambiamos la forma de añadir el elemento click, utilizando el EventAddListener

--- Versión_1/main.js
+++ Versión_1/main.js
@@ -1,7 +1,5 @@
 // Función que nos permite rellenar la pantalla
 // document.calculator.displayPanel.value (es lo que se muestra en la pantalla)
-
-
 function fillDisplay(inputvalue) {
     //Verificamos si la pantalla tiene un '0'. En caso afirmativo vaciamos la pantalla (cadena vacía '')
     if (document.calculator.displayPanel.value === '0')
@@ -18,3 +16,26 @@ function doOperation () {
         document.calculator.displayPanel.value = eval(document.calculator.displayPanel.value);
     }
 }
+
+//Recuperamos todos los inputs del DOM
+let buttons = document.querySelectorAll("input"); //obtenemos todos los botones
+buttons.forEach(function(pButton) { //pbutton-->parámetro cada botón/con forEach recorro all botones
+    if(pButton.type !== "reset" && pButton.type !=="textField"){ //descartamos reset y display
+        if (pButton.value === '='){
+            pButton.addEventListener("click", function(){ //añadimos evento click al botón =
+                doOperation();
+            });
+        }
+        else if (pButton.value === 'X'){
+            pButton.addEventListener("click", function(){ //añadimos evento click al botón X
+                fillDisplay('*');
+            });
+        }
+        else {
+            pButton.addEventListener("click", function(){ //añado evento clik al resto de botones
+                fillDisplay(pButton.value);
+            });
+        }
+
+    }
+});
